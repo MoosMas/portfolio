85e50060ccb18c2ec77c4df03752ce24f8de9f63
Sam Brands <d286050@edu.rocwb.nl>
2022-09-17 13:26:20 +0200

    Updated FullView files in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/fullview.css', 'resources/css/normalize.css', 'resources/js/fullview.js'],
+            input: [
+				'resources/css/fullview.css',
+				'resources/js/fullview.js',
+				'node_modules/fullview/dist/fullview.css',
+				'node_modules/fullview/dist/fullview.js'
+			],
             refresh: true,
         }),
     ],
