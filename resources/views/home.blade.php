2e5153fc6857d5c8b3ef6f45a0428a9340e0ba45
Sam Brands <d286050@edu.rocwb.nl>
2022-09-18 15:34:50 +0200

    Added demo content to homepagine
    This is just the code copied from the FullView demo. This is what I'll be using as a template to fill with my content.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,33 @@
 	@vite(['resources/css/fullview.css', 'resources/js/fullview.js'])
 </head>
 <body class="antialiased">
-	
+	<div id="fullview">
+		<div id="section1" class="active" data-tooltip="Section 1 Title">
+			<h2>Section 1</h2>
+			<button title="Down" id="down"><span>Click Here</span></button>
+		</div>
+		<div id="section2" data-tooltip="Section 2 Title">
+			<h2>Section 2</h2>
+			<div class="form">
+				<form>
+					<label for="select">Choose Section To Scroll</label>
+					<select name="section" id="select">
+						<option value="0">Section: 1</option>
+						<option value="1">Section: 2</option>
+						<option value="2">Section: 3</option>
+						<option value="3">Section: 4</option>
+					</select>
+				</form>
+			</div>
+		</div>
+		<div id="section3" data-tooltip="Section 3 Title">
+			<h2>Section 3</h2>
+		</div>
+		<div id="section4" data-tooltip="Section 4 Title">
+			<h2>Section 4</h2>
+		</div>
+	</div>
+
+
 </body>
 </html>
