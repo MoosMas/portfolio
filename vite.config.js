292c456106b826227091e1dfccafdaf5406a0bd6
Sam Brands <d286050@edu.rocwb.nl>
2022-09-19 11:39:16 +0200

    Added package for 3D parallax effect

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
 				'resources/css/app.css',
 				'resources/css/fullview.css',
+				'resources/js/app.js',
 				'resources/js/fullview.js',
 				'node_modules/fullview/dist/fullview.css',
 				'node_modules/fullview/dist/fullview.js'
