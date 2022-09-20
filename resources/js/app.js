a31db52cda23ffd491982ed3ba3216650af7b971
Sam Brands <d286050@edu.rocwb.nl>
2022-09-20 10:19:15 +0200

    Added parallax effect to first section

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import VanillaTilt from 'vanilla-tilt';
+
+VanillaTilt.init(document.querySelector(".parallax"), {
+	reverse: true,
+	max: 8,
+	speed: 3000,
+	transition: true,
+	reset: true,
+	glare: true,
+	'full-page-listening': true,
+	'max-glare': 0.5,
+});
