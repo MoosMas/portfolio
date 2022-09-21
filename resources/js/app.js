6089448bd76f9a8aea2616d8365eb33522cee93c
Sam Brands <d286050@edu.rocwb.nl>
2022-09-21 14:38:21 +0200

    Updated parallax effect settings

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap';
 import VanillaTilt from 'vanilla-tilt';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 VanillaTilt.init(document.querySelector(".parallax"), {
 	reverse: true,
-	max: 8,
+	max: 15,
 	speed: 3000,
+	perspective: 1000,
 	transition: true,
 	reset: true,
 	glare: true,
