ace204b113b7318583263d2cbeb8de15a268ac0d
Sam Brands <d286050@edu.rocwb.nl>
2022-09-21 14:33:31 +0200

    Added social icons
    Icons for GitHub, LinkedIn and email. Email address not included because I don't have a professional email (yes).

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,19 @@
 		<div id="section1" class="active" data-tooltip="Section 1 Title">
 			<div class="glass welcome parallax">
 				<h2>Sam's Brandende Portfolio</h2>
+
+				<div class="parallax-inner social-icons">
+					<a href="https://github.com/MoosMas" target="_blank">
+						<i class="bi bi-github inner"></i>
+					</a>
+					<a href="https://www.linkedin.com/in/sambrands/" target="_blank">
+						<i class="bi bi-linkedin"></i>
+					</a>
+					<a href="javascript:function(e){e.preventDefault();}">
+						<i class="bi bi-envelope-fill"></i>
+					</a>
+				</div>
+
 			</div>
 			<a class="icon-scroll" id="down"></a>
 		</div>
