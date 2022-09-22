8575b37478ee60d4cb5f22dcfce8baa09479df3d
Sam Brands <d286050@edu.rocwb.nl>
2022-09-22 16:00:06 +0200

    Installed Laravel Breeze
    Installed Breeze for authentication.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,7 @@
 import './bootstrap';
-import VanillaTilt from 'vanilla-tilt';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
-VanillaTilt.init(document.querySelector(".parallax"), {
-	reverse: true,
-	max: 15,
-	speed: 3000,
-	perspective: 1000,
-	transition: true,
-	reset: true,
-	glare: true,
-	'full-page-listening': true,
-	'max-glare': 0.5,
-});
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
