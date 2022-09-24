97df32d63bfc4309aef37f0780af06150baf20fe
Sam Brands <d286050@edu.rocwb.nl>
2022-09-24 12:04:49 +0200

    Added highlight column for projects

--- database/migrations/2022_09_23_064603_create_projects_table.php
+++ database/migrations/2022_09_23_064603_create_projects_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
+            $table->boolean('is_highlighted')->default(false);
             $table->string('image_path');
             $table->timestamps();
         });
