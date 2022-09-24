f62d5d998d35db5a1db9096de49c533970d7a1ed
Sam Brands <d286050@edu.rocwb.nl>
2022-09-24 21:16:59 +0200

    Added route group for projects

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::resource('projects', ProjectController::class);
+});
 
 require __DIR__.'/auth.php';
