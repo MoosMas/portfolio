3d16f34df331aacf94a0a6b6e10e62ef6b5cda9d
Sam Brands <d286050@edu.rocwb.nl>
2022-09-26 11:13:40 +0200

    Added images method to project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Models;
+
 use App\Models\Tag;
+use App\Models\Image;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,8 +11,14 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
-    
-    public function tags() {
+
+    public function tags()
+    {
         return $this->hasMany(Tag::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
     }
 }
