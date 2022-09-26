68873dd97facaec5f597cd4ebdf3b17909c1eaf8
Sam Brands <d286050@edu.rocwb.nl>
2022-09-26 11:13:56 +0200

    Removed image path column from projects table

--- database/migrations/2022_09_23_064603_create_projects_table.php
+++ database/migrations/2022_09_23_064603_create_projects_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->boolean('is_highlighted')->nullable()->default(false);
-            $table->string('image_path');
             $table->timestamps();
         });
     }
