1ecab369dfa3d47e00be27e9bad45d92f9ce6ca7
Sam Brands <d286050@edu.rocwb.nl>
2022-09-27 11:42:42 +0200

    Changed carousel if statements to inline

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -13,30 +13,18 @@
 
 								<div class="carousel-indicators">
 									@for($i = 0; $i < count($project->images); $i++)
-										@if($i == 0)
-											<button type="button" data-bs-target="#carouselExampleIndicators"
-											        data-bs-slide-to="{{$i}}" class="active" aria-current="true"
-											        aria-label="Slide {{$i}}"></button>
-										@else
-
-											<button type="button" data-bs-target="#carouselExampleIndicators"
-											        data-bs-slide-to="{{$i}}" aria-label="Slide {{$i}}"></button>
-										@endif
+										<button type="button" data-bs-target="#carouselExampleIndicators"
+										        data-bs-slide-to="{{$i}}" @if($i == 0) class="active"
+										        aria-current="true" @endif
+										        aria-label="Slide {{$i}}"></button>
 									@endfor
 								</div>
 								<div class="carousel-inner">
 									@for($i = 0; $i < count($project->images); $i++)
-										@if($i == 0)
-											<div class="carousel-item active">
+											<div class="carousel-item @if($i == 0) active @endif">
 												<img src="{{$project->images[$i]->path}}" class="d-block w-100"
 												     alt="...">
 											</div>
-										@else
-											<div class="carousel-item">
-												<img src="{{$project->images[$i]->path}}" class="d-block w-100"
-												     alt="...">
-											</div>
-										@endif
 									@endfor
 								</div>
 
